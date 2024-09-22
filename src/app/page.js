d9bca74f7a617dec725f8637f2fe24a35a8ab3c0
Eric-Mai-25 <ericmai2@gmail.com>
2024-09-22 13:36:16 -0700

    initiated calendar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import CalendarComponent from './components/CalendarComponent.js';
+"use client"
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin, {Draggable, DropArg} from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import WeeklyCalendar from './components/CalendarComponent.js';
 
 export default function Home() {
   return (
-    <div>
-      <h1>My Calendar App</h1>
-      <CalendarComponent />
-    </div>
+    <>
+    <nav className='flex justify-between mb-12 border-b border-violet-100 p-4'>
+      <h1 className='font-bol text-2xl text-gray-700'>Calendar</h1>
+    </nav>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='grid grid-cols-10'>
+        <div className='col-span-8'>
+          <FullCalendar plugins={[
+            dayGridPlugin,
+            interactionPlugin,
+            timeGridPlugin
+          ]} headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'resourceTimelineWook, dayGridMonth, timeGridWeek'
+          }}
+          
+          events={{}}
+          nowIndicator={true}
+          editable={true}
+          droppable={true}
+          selectable={true}
+          selectMirror={true}
+          // dateClick={{}}
+          // drop={}
+          // eventClick={}
+          />
+        </div>
+      </div>
+    </main>
+    </>
   );
 }
