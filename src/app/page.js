d9e8c0593f05dded5d65b091b8e1e6fe8342e3fc
Eric-Mai-25 <ericmai2@gmail.com>
2024-09-28 21:24:13 -0700

    css

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,30 @@ export default function Home() {
       <h1 className='font-bol text-2xl text-gray-700'>Calendar</h1>
     </nav>
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      
+      <div className='grid grid-cols-10'>
+        <div className='col-span-8'>
+          <FullCalendar plugins={[
+            dayGridPlugin,
+            interactionPlugin,
+            timeGridPlugin
+          ]} headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'resourceTimelineWook, dayGridMonth, timeGridWeek'
+          }}
+          
+          events={{}}
+          nowIndicator={true}
+          editable={true}
+          droppable={true}
+          selectable={true}
+          selectMirror={true}
+          // dateClick={{}}
+          // drop={}
+          // eventClick={}
+          />
+        </div>
+      </div>
     </main>
     </>
   );
