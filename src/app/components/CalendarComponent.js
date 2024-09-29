86903c93b467467f9553d8f00b8397b0ab23bcc0
Eric-Mai-25 <ericmai2@gmail.com>
2024-09-28 21:24:51 -0700

    calendar own component

--- src/app/components/CalendarComponent.js
+++ src/app/components/CalendarComponent.js
@@ -7,7 +7,27 @@ export default function Calendar() {
 
   return (
     <div>
-
+      <FullCalendar plugins={[
+            dayGridPlugin,
+            interactionPlugin,
+            timeGridPlugin
+          ]} headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'resourceTimelineWook, dayGridMonth, timeGridWeek'
+          }}
+          
+          events={{}}
+          nowIndicator={true}
+          editable={true}
+          droppable={true}
+          selectable={true}
+          selectMirror={true}
+          // dateClick={{}}
+          // drop={}
+          // eventClick={}
+          />
+        </div>
 
     </div>
   );
